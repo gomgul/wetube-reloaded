4850b492d3cea576f5a4ec7709f5f2805612610d
gom <nobrain908@gmail.com>
2021-09-17 20:51:38 +0900

    upgrade

---
+++ src/routers/globalRouter.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {join, login} from "../controllers/userController";
+import {trending, search} from "../controllers/videoController"; //export의 default값이 없어서 import {  } 괄호가 필요함
+
+
+
+const globalRouter = express.Router();
+
+
+
+globalRouter.get("/",trending);
+globalRouter.get("/join",join);
+globalRouter.get("/login",login);
+globalRouter.get("/search",search);
+
+
+
+export default globalRouter; //default는 내가 원하는 이름으로 import 설정할수 있음
--- src/server.js
+++ src/server.js
@@ -1,19 +1,56 @@
 import express from "express";
+import morgan from "morgan"; // middleware를 return 해준다
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+
+
 
 const handleHome = (req, res) => {
-    return res.send("it's better than ever");
+    console.log("I will respond");
+    return res.send("Hello");
 };
-const handleLogin = (req, res) => {
-    return res.send("Login here");
+const login = (req, res) => {
+    return res.send("login");
 };
 
+/*
+const routerLogger = (req, res, next) => {
+    console.log("PATH", req.path);
+    next();
+};
+const methodLogger = (req, res, next) => {
+    console.log("METHOD", req.method);
+    next();
+};
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if ( url === "/protected") {
+        return res.send("<h1> Not Allowed </h1>")
+    }
+    console.log("Allowed, you may continue");
+    next();
+};
+const handelProtected = (req, res, next) => {
+    return res.send("welcome to the private lounge");
+}
 
-app.get("/", handleHome);
-app.get("/Login", handleLogin);
+*/
+
+
+
+
+app.use(logger);
+
+app.get("/", handleHome); //routes만들고 controllers 만든 것
+app.get("/login", login);
+/*
+app.get("/", (req,res) => res.send("hello"));
+app.use(logger);
+app.get("/protected", handelProtected);
+*/
 
 const handleListening = () => 
 console.log(`Server listening on port http://localhost:${PORT}🙄`);
