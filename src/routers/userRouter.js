c8e49144eb67cc264372f03b0b977915fd1f5657
gom <nobrain908@gmail.com>
2021-10-04 23:43:06 +0900

    #7.23

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,22 @@
 import express from "express";
-import { edit, remove, logout, see  } from "../controllers/userController";
+import {
+    edit,
+    getEdit,
+    postEdit,
+    logout,
+    see,
+    startGithubLogin,
+    finishGithubLogin,
+  } from "../controllers/userController";
 
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
+userRouter.route("/edit").get(getEdit).post(postEdit);
+
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;
