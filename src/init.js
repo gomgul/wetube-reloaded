e5469db738f0977cd7916152b10712eb5feb4438
gom <nobrain908@gmail.com>
2021-10-06 00:53:16 +0900

    #8.0

--- src/init.js
+++ src/init.js
@@ -7,6 +7,6 @@ import app from "./server";
 const PORT = 4000;
 
 const handleListening = () =>
-  console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
+  console.log(`✅ Server listenting on http://localhost:${PORT} 🍕🎅`);
 
 app.listen(PORT, handleListening);
--- src/models/User.js
+++ src/models/User.js
@@ -13,10 +13,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function () {
-    
     this.password = await bcrypt.hash(this.password, 5);
-    
-  
   });
 
 
