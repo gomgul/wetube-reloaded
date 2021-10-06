44ef8f000c50c180f7f2d75a85f7c6c18c137d9e
gom <nobrain908@gmail.com>
2021-10-07 00:35:32 +0900

    #8.0

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,7 +7,7 @@ import {
     postEdit,
     postUpload,
   } from "../controllers/videoController";
-import { protectorMiddleware } from "../middlewares";
+import { protectorMiddleware,videoUpload  } from "../middlewares";
 
 
 
@@ -30,7 +30,7 @@ videoRouter
   .route("/upload")
   .all(protectorMiddleware)
   .get(getUpload)
-  .post(postUpload);
+  .post(videoUpload.single("video"), postUpload);
 
 
 export default videoRouter;
